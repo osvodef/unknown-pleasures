656febb99c726282f002de2380d094b6e1374815
Alex Fedosov <osvodef@gmail.com>
2019-12-14 03:03:11 +0700

    refactor shaders a bit

--- src/shaders/area.vert.glsl
+++ src/shaders/area.vert.glsl
@@ -9,12 +9,12 @@ uniform float maxHeight;
 uniform vec2 screenSize;
 
 void main() {
-    vec2 position_screen = vec2(
+    vec2 positionScreen = vec2(
         xLeft + (xRight - xLeft) * position.x,
         yOffset + position.y * maxHeight
     );
 
-    vec2 position_ndc = (position_screen) / (screenSize / 2.0) - vec2(1.0, 1.0);
+    vec2 positionNdc = (positionScreen) / (screenSize / 2.0) - vec2(1.0, 1.0);
 
-    gl_Position = vec4(position_ndc, 0.0, 1.0);
+    gl_Position = vec4(positionNdc, 0.0, 1.0);
 }
--- src/shaders/line.vert.glsl
+++ src/shaders/line.vert.glsl
@@ -11,14 +11,14 @@ uniform float maxHeight;
 uniform vec2 screenSize;
 
 void main() {
-    vec2 position_screen = vec2(
+    vec2 positionScreen = vec2(
         xLeft + (xRight - xLeft) * position.x,
         yOffset + position.y * maxHeight
     );
 
-    vec2 aspect_ratio = vec2(maxHeight / (xRight - xLeft), 1.0);
-    vec2 normal_screen = normalize(normal * aspect_ratio) * (width / 2.0);
-    vec2 position_ndc = (position_screen + normal_screen) / (screenSize / 2.0) - vec2(1.0, 1.0);
+    vec2 aspectRatioCorrection = vec2(maxHeight, xRight - xLeft);
+    vec2 normalScreen = normalize(normal * aspectRatioCorrection) * (width / 2.0);
+    vec2 positionNdc = (positionScreen + normalScreen) / (screenSize / 2.0) - vec2(1.0, 1.0);
 
-    gl_Position = vec4(position_ndc, 0.0, 1.0);
+    gl_Position = vec4(positionNdc, 0.0, 1.0);
 }
